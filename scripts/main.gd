28c8858b7cccc353a5b25ce3ee0f5f2be2ab7493
Vollachia <ayhammuati@gmail.com>
2024-09-02 08:47:06 +0300

    added a restart button that teleports the player to start position

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,18 +1,19 @@
 extends Node2D
 var piano
 var player
-
+var playerInst
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	piano = preload("res://scenes/piano.tscn")
 	var pianoInst = piano.instantiate()
 	add_child(pianoInst)
 	player = preload("res://scenes/player.tscn")
-	var playerInst = player.instantiate()
+	playerInst = player.instantiate()
 	playerInst.position.x = 80
 	playerInst.position.y = 50
 	add_child(playerInst)
 
-func _prrocess():
+func _process(_delta):
 	if Input.is_action_pressed("restart"):
-		_ready()
+		playerInst.position.x = 80
+		playerInst.position.y = 50
