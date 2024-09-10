2a3ed2c24c354be5f360a3a33c15336802664187
Vollachia <ayhammuati@gmail.com>
2024-09-10 20:07:10 +0300

    worked more on the drawing notes aspect, added function to find rect2 pos

--- scripts/piano.gd
+++ scripts/piano.gd
@@ -7,10 +7,31 @@ const FONT := preload("res://assets/fonts/Kenney Future Narrow.ttf")
 var _keys: Array = []
 var _notes: PackedStringArray = PackedStringArray()
 
-var _activeKey: int = -1
+var _active_key: int = -1
+
+@export var key_amount: int = 25
+@export var key_width: int = 4
+@export var key_height: int = 8
+
+var _total_key_width = key_width * key_amount
+var _viewport_size = get_viewport().get_visible_rect().size
+
+func _find_key_pos(_index: int) -> Vector2:
+      # viewport size height - key height = initial position y
+      # (viewport size width - total key width)/2+(_index-1)*key_width is the x position
+      # size is just gonna be key_width and key_height
+      var x = _viewport_size.y - key_height
+      var y = (_viewport_size.x - _total_key_width) / 2 + (_index * key_width)
+      var key_position := Vector2(x, y)
+      return key_position
+
+
+func _activate_key(_index: int) -> void:
+      _active_key = _index
+
 
 class Key:
-      var area: Rect2 = Rect2()
+      var area := Rect2()
       var note: String = ""
       var index: int = -1
       
