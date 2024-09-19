6a6a67d6e7c0ad2ca57b87ae60c85d50952fb9ed
Vollachia <ayhammuati@gmail.com>
2024-09-19 18:59:46 +0300

    made it so that the notes are centere

--- scripts/piano.gd
+++ scripts/piano.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
 @export var pianoScale = [1, 3, 5, 6, 8, 10, 12, 13]
-@export var startSpot = 3
+@export var keyAmount = 5
+var viewport_width
+var viewport_height
 
 var key = preload("res://scenes/key.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var spot = startSpot
+	var viewport = get_viewport().get_visible_rect().size
+	viewport_width = viewport.x
+	viewport_height = viewport.y
+	var keySize: int = 16
+	var totalKeyWidth = keySize * keyAmount
+	var spot = ((viewport_width - totalKeyWidth) / 2) / keySize
 	for pitch in pianoScale:
 		var keyInst = key.instantiate()
 		keyInst.setupKey(spot, pitch, "white")
